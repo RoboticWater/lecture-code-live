f8430421164d8c8493a99dde1c48db2f08323ac4
RoboticWater <jakebritti@gmail.com>
2018-09-17 14:21:34 -0400

    refactor server to top folder

---
+++ node_modules/mongoose-gridfs/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+/* dependencies */
+const path = require('path');
+const storage = require(path.join(__dirname, 'lib', 'storage'));
+
+
+/* export gridfs storage */
+module.exports = storage;
