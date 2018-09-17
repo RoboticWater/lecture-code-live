c469eec144467a50da15f4a8f5cdb69dca8e255a
RoboticWater <jakebritti@gmail.com>
2018-09-17 16:14:45 -0400

    fixed client production build

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -193,6 +193,7 @@ module.exports = {
                         ident: 'postcss',
                         plugins: () => [
                           require('postcss-flexbugs-fixes'),
+                          require('precss'),
                           autoprefixer({
                             browsers: [
                               '>1%',
--- server/app.js
+++ server/app.js
@@ -3,14 +3,20 @@ import bodyParser from'body-parser';
 import methodOverride from'method-override';
 import WebSocket from 'ws';
 import util from 'util';
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import path from 'path';
 
 import routesApi from './routes';
 
 const app = express();
+app.use(express.static(path.join(__dirname, '/../client/build')))
 
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 app.use('/api', routesApi);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+})
 
 export default app;
