235de21d37188251eff129b0844ea8f1dd3fadc8
RoboticWater <jakebritti@gmail.com>
2018-09-24 17:54:09 -0400

    changed websocket output

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const db = new Database(process.env.MONGODB_URI, process.env.MONGODB_URI);
 
 var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 8080})
+  wss = new WebSocketServer({port: 3001})
 
 wss.on('connection', (ws) => {
   console.log('[socket] connection detected');
